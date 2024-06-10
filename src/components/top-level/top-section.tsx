65c59939ca21acf8d4849c57ffa04c0755742dee
Scythe504 <hellfireprince98@gmail.com>
2024-06-10 14:09:32 +0530

    Top-section-mobile

--- src/components/top-level/top-section.tsx
+++ src/components/top-level/top-section.tsx
@@ -1,5 +1,6 @@
 import dice from '../../assets/mobile-fiest.png';
 import wa from '../../assets/wa.svg'
+import { Button } from '../ui/button';
 export default function TopSection() {
     return <div>
         <div className='h-full flex flex-col justify-center items-center w-full gap-6'>
@@ -10,12 +11,22 @@ export default function TopSection() {
                 />
             </div>
         </div>
-        <div>
-            <h1 className='text-orange-600 text-5xl text-left p-2'>Play & Earn</h1>
-            <p>Multiplayer Gaming Whatsapp Group Platform <br />
+        <div className='flex flex-col gap-3'>
+            <h1 className='text-orange-600 text-5xl text-left'>Play & Earn</h1>
+            <p className='text-left text-xl p-1'>Multiplayer Gaming Whatsapp Group Platform
                 to earn real money
-                <a href='todo' className='flex flex-row space-x-1'><img src={wa} alt='whatsapp icon'/>Join Now</a>
             </p>
+            <Button className=' bg-clip-border bg-white rounded-md p-4 ml-1 w-fit'>
+                <a href='https://wa.link/hdv7d0' 
+                    target='_blank'
+                className='flex flex-row space-x-1 object-contain items-center'>
+                    <img src={wa} alt='whatsapp icon' className='text-white'
+                        height={30}
+                        width={30}
+                    />
+                    Join Now
+                </a>
+            </Button>
         </div>
     </div>
 }
