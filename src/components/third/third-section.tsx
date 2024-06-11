240b73b664b929b84e68a4cfc16bcd4b9e2a3543
Scythe504 <hellfireprince98@gmail.com>
2024-06-11 13:36:23 +0530

    final

--- src/components/third/third-section.tsx
+++ src/components/third/third-section.tsx
@@ -6,9 +6,9 @@ export default function ThirdSection() {
                 <img src={mountain} alt='dice' />
             </div>
             <div id='withdraw' className='bg-[#a8925b] w-screen -mt-8 z-10 pb-16 sm:px-24 h-[350px] px-2'>
-                <h1 className='text-4xl text-center'>How to Withdraw</h1>
+                <h1 className='text-4xl text-center text-black'>How to Withdraw</h1>
                 <br />
-                <p className='ml-1 text-xl'>Watch this YouTube video:<a href="" target='_blank' className='underline text-orange-500 bg-black bg-clip-text'>Click here</a></p>
+                <p className='ml-1 text-xl'>Watch this YouTube video:<a href="https://youtube.com/shorts/n6cNr-mpTWo?si=98sniW82ZxMHfczp" target='_blank' className='underline text-orange-500 bg-black bg-clip-text'>Click here</a></p>
                 <br/>
                 <div className='text-left gap-2 flex flex-col text-xl'>
                     <p>Step 1:- Direct Message to admin,
