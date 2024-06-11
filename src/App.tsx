4f3d49cdbe5e7c8afcb472c7076f8f00b0d448f3
Scythe504 <hellfireprince98@gmail.com>
2024-06-11 13:16:04 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { ThemeProvider } from './components/theme-provider';
+import PDF_File from './components/terms-conditions/terms-conditions';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
 import About from './components/about/about';
 import HowToPlay from './components/how-to-play/play';
 import Withdraw from './components/withdraw/withdraw';
 import Join from './components/join/join';
-import { lazy } from 'react';
-const PDF_File = lazy(()=> import('@/components/terms-conditions/terms-conditions'))
+
 function App() {
 
   return (
@@ -17,7 +17,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/rules' element={<PDF_File />} />
-            <Route path='/contact' element={<About/>}/>
+            <Route path='/about' element={<About/>}/>
             <Route path='/how-to-play' element={<HowToPlay/>}/>
             <Route path='/withdraw' element={<Withdraw/>}/>
             <Route path='/wa' element={<Join/>}/>
