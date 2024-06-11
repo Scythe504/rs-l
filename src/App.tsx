6f38a9bbf7e59268abe7a7fec171a343753b1164
Scythe504 <hellfireprince98@gmail.com>
2024-06-11 13:15:22 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ import About from './components/about/about';
 import HowToPlay from './components/how-to-play/play';
 import Withdraw from './components/withdraw/withdraw';
 import Join from './components/join/join';
-import { lazy, Suspense } from 'react';
-import { Loader } from './components/loading/loading';
-const PDF_File = lazy(() => import('@/components/terms-conditions/terms-conditions'))
+import { lazy } from 'react';
+const PDF_File = lazy(()=> import('@/components/terms-conditions/terms-conditions'))
 function App() {
 
   return (
@@ -16,14 +15,12 @@ function App() {
       <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
         <main className='font-satoshi-bold flex flex-col min-w-screen overflow-hidden px-8'>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Home />}/>
             <Route path='/rules' element={<PDF_File />} />
-            <Route path='/contact' element={<About />} />
-            <Route path='/how-to-play' element={<HowToPlay />} />
-            <Route path='/withdraw' element={<Withdraw />} />
-            <Suspense fallback={<Loader/>}>
-              <Route path='/wa' element={<Join />} />
-            </Suspense>
+            <Route path='/contact' element={<About/>}/>
+            <Route path='/how-to-play' element={<HowToPlay/>}/>
+            <Route path='/withdraw' element={<Withdraw/>}/>
+            <Route path='/wa' element={<Join/>}/>
           </Routes>
         </main>
       </ThemeProvider>
